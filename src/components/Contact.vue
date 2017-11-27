4ece8dbb51a30f965d10f2942a54ff9044ca32f4
renanbronchart <renan.bronchart@gmail.com>
2017-11-28 00:14:21 +0100

    [Feat:Contact] Add component contact and route contact

---
+++ src/components/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='contact'>
+    <div class='container'>
+      <h3 class='page__title'>Contact</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  .page__title {
+    margin: 2rem 0 0 0;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Home from '@/components/Home'
+import Contact from '@/components/Contact'
 import Chapiteau from '@/components/Chapiteau'
 
 Vue.use(Router)
@@ -19,6 +20,11 @@ export default new Router({
       path: '/chapiteau/:id',
       name: 'Chapiteau',
       component: Chapiteau
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     },
     {
       path: '/*',
